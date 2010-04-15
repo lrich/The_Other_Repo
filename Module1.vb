6683dcd0e161031c05be3e5c830564425765fe2f
lrich <leoncrich@gmail.com>
2010-04-15 11:14:20 -0400

    version 2 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- Module1.vb
+++ Module1.vb
@@ -3,6 +3,8 @@
     Sub Main()
         Console.WriteLine("Hello World!")
         Console.WriteLine("Have fine day!")
+        Console.WriteLine("Help, I'm a rock!")
+        Console.WriteLine("It can't happen here!")
     End Sub
 
 End Module
